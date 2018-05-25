eeb7bd843faa06faa2e98dbbfa00a96ffd14a3fc
Jordan Walker <jwalker2276@gmail.com>
2018-05-24 20:05:08 -0500

    Changed layout and styles of edit item component.

--- src/components/EditItem.js
+++ src/components/EditItem.js
@@ -15,7 +15,6 @@ class EditItem extends React.Component {
     const itemKey = this.props.itemKey;
     const listKey = this.props.selectedList;
     const lists = this.props.lists;
-
     // Check to make sure the listItem key is not null
     if (itemKey === null) {
       return <p>item was null</p>;
@@ -23,33 +22,82 @@ class EditItem extends React.Component {
 
     // Get the selected item to edit
     const selectedListItem = lists[listKey].items[itemKey];
+    console.log(selectedListItem);
     return (
-      <div className="editForm">
-        <p className="edit-form-header">Change item info</p>
-        <label>Name</label>
-        <input
-          type="text"
-          name="name"
-          value={selectedListItem.name}
-          onChange={this.handleEvent}
-        />
-        <label>Link</label>
-        <input
-          type="text"
-          name="link"
-          value={selectedListItem.link}
-          onChange={this.handleEvent}
-        />
-        <label>Cost</label>
-        <select
-          name="cost"
-          value={selectedListItem.cost}
-          onChange={this.handleEvent}
-        >
-          <option value="paid">Paid</option>
-          <option value="free">Free</option>
-        </select>
-        <button onClick={() => this.props.flagItemForEdit(null)}>Done</button>
+      <div className="edit-listitem-div">
+        <p className="item-comment">// Change your items values</p>
+        <div className="edit-listitem-grid">
+          <div className=" add-listitem-line-one">
+            <label>
+              <span className="item-class">class </span>
+            </label>
+            <input
+              type="text"
+              name="name"
+              value={selectedListItem.name}
+              onChange={this.handleEvent}
+            />
+            <span className="item-class">
+              extends{" "}
+              <span className="item-title">
+                selectedListItem.list <span className="item-text">&#123;</span>
+              </span>
+            </span>
+          </div>
+          {/* end of line one */}
+          <div className="add-listitem-line-two item-indent">
+            <label className="item-class">
+              constructor{" "}
+              <span className="item-text"> (cost, link) &#123;</span>
+            </label>
+          </div>
+          {/* end of line two */}
+          <div className="add-listitem-line-three item-indent">
+            <label className="item-this item-indent">
+              this<span className="item-period">.</span>
+              <span className="item-text">link</span>
+              <span className="item-equal"> = </span>
+            </label>
+            <input
+              type="text"
+              name="link"
+              value={selectedListItem.link}
+              onChange={this.handleEvent}
+            />
+            <span className="item-text">;</span>
+          </div>
+          {/* end of line three */}
+          <div className="add-listitem-line-four item-indent">
+            <label className="item-this item-indent">
+              this<span className="item-period">.</span>
+              <span className="item-text">cost</span>
+              <span className="item-equal"> = </span>
+            </label>
+            <select
+              name="cost"
+              value={selectedListItem.cost}
+              onChange={this.handleEvent}
+            >
+              <option value="paid">Paid</option>
+              <option value="free">Free</option>
+            </select>
+            <span className="item-text">;</span>
+          </div>
+          {/* end of line four */}
+          <div className="add-listitem-line-five">
+            <label className="form-closing item-text item-indent">&#125;</label>
+          </div>
+          {/* end of line five */}
+          <div className="add-listitem-line-six">
+            <p className="item-text">&#125;</p>
+          </div>
+          <button
+            className="syntax-btn"
+            onClick={() => this.props.flagItemForEdit(null)}
+          >
+            Done
+          </button>
+        </div>
       </div>
     );
   }

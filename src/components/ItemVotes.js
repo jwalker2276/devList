bc9a19db6042817471f6b157ec866efb19274b3d
Jordan Walker <jwalker2276@gmail.com>
2018-05-26 21:43:03 -0500

    Added React Animations into itemvotes component.

--- src/components/ItemVotes.js
+++ src/components/ItemVotes.js
@@ -1,7 +1,25 @@
 import React from "react";
+import { TransitionGroup, CSSTransition } from "react-transition-group";
 
 const ItemVotes = props => (
-  <p className="item-text item-indent"><span className="item-this">this</span><span className="item-period">.</span>totalVotes<span className="item-equal"> = </span><span className="item-number">{`${props.totalVotes}`}</span>&#59;</p>
+  <p className="item-text item-indent">
+    <span className="item-this">this</span>
+    <span className="item-period">
+      .
+    </span>totalVotes<span className="item-equal"> = </span>
+    <TransitionGroup component="span" className="fade">
+      <CSSTransition
+        classNames="fade"
+        key={props.totalVotes}
+        timeout={{ enter: 500, exit: 0 }}
+      >
+        <span className="item-number">
+          {`${props.totalVotes}`}
+          <span className="item-text">&#59;</span>
+        </span>
+      </CSSTransition>
+    </TransitionGroup>
+  </p>
 );
 
 export default ItemVotes;

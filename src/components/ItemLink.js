402bbe2de0649c144aca684f81f942f248a8b7cd
Jordan Walker <jwalker2276@gmail.com>
2018-06-03 14:44:30 -0500

    Added target to links.

--- src/components/ItemLink.js
+++ src/components/ItemLink.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 const ItemLink = props => (
-  <p className="item-text item-indent"><span className="item-this">this</span><span className="item-period">.</span>link<span className="item-equal"> = </span><span className="item-string">"</span><a className="item-link" href={`${props.link}`}>{`${props.link}`}</a><span className="item-string">"</span>&#59;</p>
+  <p className="item-text item-indent">
+    <span className="item-this">this</span>
+    <span className="item-period">.</span>link<span className="item-equal">
+      {" "}
+      ={" "}
+    </span>
+    <span className="item-string">"</span>
+    <a className="item-link" href={`${props.link}`} target="_blank">{`${
+      props.link
+    }`}</a>
+    <span className="item-string">"</span>&#59;
+  </p>
 );
 
 export default ItemLink;

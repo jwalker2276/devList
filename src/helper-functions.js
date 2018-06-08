9e4a6f6a4b41621fb4d2c4ae59f26b6209ff86c3
Jordan Walker <jwalker2276@gmail.com>
2018-06-07 19:51:26 -0500

    Fixed formatName fx to remove special characters and pass jest test.

--- src/helper-functions.js
+++ src/helper-functions.js
@@ -1,6 +1,7 @@
-// Function formats a string removing spaces and uppercase characters
+// Function formats a string removing spaces,uppercase and special characters
 // Arguments : string
 // Return: string
 export function formatName(name) {
-  return name.toLowerCase().replace(/\s/g, "");
+  // return name.toLowerCase().replace(/\s/g, "");
+  return name.toLowerCase().replace(/[^a-z]/g, "");
 }
